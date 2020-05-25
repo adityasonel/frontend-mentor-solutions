299e5908c82631aa4d7d0e48bd57710eee429395
adityasonel <aditya.sonel@gmail.com>
2020-05-25 12:35:28 +0530

    easy-bank ui completed

--- easybank-landing-page/src/js/main.js
+++ easybank-landing-page/src/js/main.js
@@ -6,13 +6,21 @@ class Main {
     constructor() {
         this.triggerMenu = document.getElementById("trigger");
         this.triggerMenu.addEventListener("change", this.toggleMenu.bind(this));
+
+        this.activeLi = document.getElementsByClassName("active")[0].parentNode;
+        this.activeLi.style.borderBottom = "4px solid hsl(136, 65%, 51%)";
+        this.activeA = document.getElementsByClassName("active")[0];
+        this.activeA.style.color = "hsl(233, 26%, 24%)";
+
+        this.navMbl = document.getElementById("nav__mbl");
     }
 
     toggleMenu(ev) {
         if (ev.target.checked) {
-            console.log("menu-on");
+            this.navMbl.style.display = "block";
+            this.navMbl.style.boxShadow = "0px 160px 100px 6px rgba(0, 0, 0, 0.42)";
         } else {
-            console.log("menu-off");
+            this.navMbl.style.display = "none";
         }
     }
 }
--- easybank-landing-page/webpack.config.js
+++ easybank-landing-page/webpack.config.js
@@ -4,41 +4,44 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
-    port: 8000,
-  },
-  entry: "./src/js/main.js",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        loader: "file-loader",
-        options: {
-          outputPath: "img",
-        },
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8000,
+    },
+    entry: "./src/js/main.js",
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            favicon: "./src/images/favicon-32x32.png",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|ico)$/i,
+                loader: "file-loader",
+                options: {
+                    outputPath: "img",
+                },
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
 };
